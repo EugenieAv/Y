04d740c8a218f2aee496cb436e1dc5ac64474fd4
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-02-09 21:59:04 +0100

    model et controlleur de restaurant et review ok pour la user storie

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "restaurants destruction ..."
+Restaurant.destroy_all if Rails.env.development?
+
+Restaurant.create!(name: "Chez Mamie", address: "La Garde", category: "chinese", phone_number: "04 94 22 33 44 ")
+Restaurant.create!(name: "Chez Maman", address: "La Garde Le Thouar" , category: "french", phone_number: "04 94 22 33 44 ")
+Restaurant.create!(name: "A la Casa", address: "Montpellier", category: "chinese", phone_number: "04 94 22 33 44 ")
+Restaurant.create!(name: "Les frères Poulard", address: "Rue du Courreau", category: "belgian", phone_number: "04 94 22 33 44 ")
+Restaurant.create!(name: "Italia", address: "Les halles Plaza", category: "italian" , phone_number: "04 94 22 33 44 ")
+
+puts "#{Restaurant.count} crés"
